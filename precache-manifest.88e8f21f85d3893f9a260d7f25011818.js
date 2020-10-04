a3de603e2ce1e2f764425282499c3290faa1287b
Wang-Yilan <1354012000@qq.com>
2020-10-04 17:14:38 +0800

    work01

---
+++ precache-manifest.88e8f21f85d3893f9a260d7f25011818.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbfea02ab5081e165de942fe0fd6040c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2574f1652b285f5c2b",
+    "url": "/static/js/2.f8e35e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8e35e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5b95eebf4818831de1e",
+    "url": "/static/js/main.28cb0765.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
